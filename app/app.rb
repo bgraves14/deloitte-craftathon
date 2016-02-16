221a1638da2862b5abf61b89c82db93eac3ebaf6
Sivan Patel <sivan.patel@googlemail.com>
2016-02-16 16:17:00 +0000

    set up different routes and tests for them

--- app/app.rb
+++ app/app.rb
@@ -6,8 +6,24 @@ require "open-uri"
 class EtsyJson < Sinatra::Base
 
   get '/price' do
-    listings = Listings.new
+    @listings = Listings.new
     listings.price.to_s
+  end
+
+  get '/quantity' do
+
+  end
+
+  get '/materials' do
+
+  end
+
+  get '/tags' do
+
+  end
+
+  get '/categories' do
+
   end
 
 end
--- spec/features/price_feature_spec.rb
+++ spec/features/price_feature_spec.rb
@@ -6,10 +6,34 @@ feature "etsy price" do
     expect(page.status_code).to eq(200)
   end
 
+  scenario 'a quantity route exists' do
+    visit '/quantity'
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario 'a materials route exists' do
+    visit '/materials'
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario 'a tags route exists' do
+    visit '/tags'
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario 'a categories route exists' do
+    visit '/categories'
+    expect(page.status_code).to eq(200)
+  end
 
   scenario 'it shows the highest price' do
     visit '/price'
     expect(page).to have_content("244")
+  end
+
+  scenario 'it shows the most common materials' do
+    visit '/materials'
+    expect(page).to have_content("gold")
   end
 
 end
