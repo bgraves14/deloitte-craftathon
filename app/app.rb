0c02a37fbffe0e8c7e1ee47ca34c312de34935b1
raphael jauslin (▰˘◡˘▰) <NULL-OPERATOR@users.noreply.github.com>
2016-02-16 21:00:19 +0000

    last fix, on app.rb

--- app/app.rb
+++ app/app.rb
@@ -5,8 +5,6 @@ require_relative 'models/listings'
 
 class EtsyJson < Sinatra::Base
 
-  @listings = Listings.new
-
   get '/:method' do
     listings = Listings.new
     listings.send(params[:method])
